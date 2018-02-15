f2f93129a6bac3957de695a7a440f3ce26dfca06
zsarna2 <zsarna2@gmail.com>
2018-02-15 15:20:52 +0100

    shop menu implement started

--- src/main/java/com/coodecool/plaza/cmdprog/CmdProgram.java
+++ src/main/java/com/coodecool/plaza/cmdprog/CmdProgram.java
@@ -103,10 +103,14 @@ public class CmdProgram {
                         }
                         break;
                     case 4:
+                        for (Shop shop : myPlaza.getShops()) {
+                            System.out.println(shop.getName());
+                        }
                         System.out.println("Which shop do you want to go in?");
                         tempShop = scan.nextLine();
                         try {
                             myShop = (ShopImpl) myPlaza.findShopByName(tempShop);
+                            runShopMenu(myShop);
                             break;
                         } catch (NoSuchShopException nsse) {
                             System.out.println(nsse.getMessage());
@@ -137,4 +141,55 @@ public class CmdProgram {
             }
         }
     }
+
+    public void runShopMenu(ShopImpl myShop) {
+        boolean running = true;
+        while (running) {
+            System.out.println("Welcome to " + myShop.getOwner() + "'s " + myShop.getName() + "!");
+            System.out.println("1) to list available products.\n" +
+                    "2) to find products by name.\n" +
+                    "3) to display the shop's owner.\n" +
+                    "4) to open the shop.\n" +
+                    "5) to close the shop.\n" +
+                    "6) to add new product to the shop.\n" +
+                    "7) to add existing products to the shop.\n" +
+                    "8) to buy a product by barcode.\n" +
+                    "0) go back to plaza");
+            String option = scan.nextLine();
+            try {
+                int subNum = Integer.parseInt(option);
+                switch (subNum) {
+                    case 1:
+                        try {
+                            System.out.println(myShop.toString());
+                        } catch (IndexOutOfBoundsException ioobe) {
+                            System.out.println("There's no item.");
+                        }
+                        break;
+                    case 2:
+                        System.out.println("Enter the item name:");
+                        String itemname = scan.nextLine();
+                        try {
+                            System.out.println(myShop.findByName(itemname).get(0).toString());
+                        } catch (IndexOutOfBoundsException ioobe) {
+                            System.out.println("No such item in this shop");
+                        }
+                        break;
+                    case 3:
+                        System.out.println("The owner of this shop is " + myShop.getOwner());
+                        break;
+                    case 4:
+                        myShop.open();
+                        System.out.println("You have opened the shop");
+                        break;
+                    case 5:
+                        myShop.close();
+                        System.out.println("You have closed the shop");
+                        break;
+                }
+        } catch (ShopIsClosedException sice) {
+                System.out.println(sice.getMessage());
+            }
+    }
+        }
 }
